889c5d385eadac843eb8d5f07a7340bf97365b8c
Your Name <alo20172@uvg.edu.gt>
2022-02-24 17:59:35 -0600

    -primer commit

---
+++ HDT4/src/src/IStack.java
@@ -0,0 +1,19 @@
+package src;
+/**
+ * 
+ * @author diego alonzo 20172
+ *Es generic y es un ADT
+ * @param <T>
+ */
+public interface IStack<T> {
+
+	void push(T value);
+	
+	T pull();
+	
+	T peek();
+	
+	int count();
+	
+	boolean isEmpty();
+}
