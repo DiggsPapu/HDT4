bd04e9ecf390dc00925a494cc30c698fed69c147
IsaacG <78927637+gut21836@users.noreply.github.com>
2022-03-02 14:51:47 -0600

    Función de postfix to infix

--- HDT4/src/pstfixInfix/Infix2Pstfix.java
+++ HDT4/src/pstfixInfix/Infix2Pstfix.java
@@ -1,5 +1,77 @@
 package pstfixInfix;
+import java.util.Stack;
 
 public class Infix2Pstfix {
+   
+  static int Prec(char ch)
+    {
+        switch (ch)
+        {
+        case '+':
+        case '-':
+            return 1;
+      
+        case '*':
+        case '/':
+            return 2;
+      
+        case '^':
+            return 3;
+        }
+        return -1;
+    }
+  
+    static String infixToPostfix(String exp)
+    {
+        // Inicializar un String vacio para el resultado
+        String result = new String("");
+         
+        // inicializar un stack vacio
+        Stack<Character> stack = new Stack<>();
+         
+        for (int i = 0; i<exp.length(); ++i)
+        {
+            char c = exp.charAt(i);
+             
+            if (Character.isLetterOrDigit(c))
+                result += c;
+              
+            else if (c == '(')
+                stack.push(c);
+             
+            else if (c == ')')
+            {
+                while (!stack.isEmpty() &&
+                        stack.peek() != '(')
+                    result += stack.pop();
+                 
+                    stack.pop();
+            }
+            else // se encontro un operador
+            {
+                while (!stack.isEmpty() && Prec(c)
+                         <= Prec(stack.peek())){
+                   
+                    result += stack.pop();
+             }
+                stack.push(c);
+            }
+      
+        }
+      
+        // sacar los operadores del stack
+        while (!stack.isEmpty()){
+            if(stack.peek() == '(')
+                return "Invalid Expression";
+            result += stack.pop();
+         }
+        return result;
+    }
+   
+    public static void main(String[] args)
+    {
+        String exp = "a+b*(c^d-e)^(f+g*h)-i";
+        System.out.println(infixToPostfix(exp));
+    }  
 
 }
